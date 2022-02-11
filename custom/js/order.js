7de13cc1c4565fe7919e5aee88be75f9a4207645
Jetsada Detwangklang <74184207+pearth13@users.noreply.github.com>
2022-02-11 12:23:17 +0700

    Add files via upload

---
+++ custom/js/order.js
@@ -0,0 +1,333 @@
+var manageOrderTable;
+// print order function
+function printOrder(orderId = null) {
+	if(orderId) {		
+			
+		$.ajax({
+			url: 'php_action/printOrder.php',
+			type: 'post',
+			data: {orderId: orderId},
+			dataType: 'text',
+			success:function(response) {
+				
+				var mywindow = window.open('', 'Stock Management System', 'height=400,width=600');
+        mywindow.document.write('<html><head><title>Order Invoice</title>');        
+        mywindow.document.write('</head><body>');
+        mywindow.document.write(response);
+        mywindow.document.write('</body></html>');
+
+        mywindow.document.close(); // necessary for IE >= 10
+        mywindow.focus(); // necessary for IE >= 10
+
+        mywindow.print();
+        mywindow.close();
+				
+			}// /success function
+		}); // /ajax function to fetch the printable order
+	} // /if orderId
+} // /print order function
+
+function addRow() {
+	$("#addRowBtn").button("loading");
+	var tableLength = $("#productTable tbody tr").length;
+	var tableRow;
+	var arrayNumber;
+	var count;
+	if(tableLength > 0) {		
+		tableRow = $("#productTable tbody tr:last").attr('id');
+		arrayNumber = $("#productTable tbody tr:last").attr('class');
+		count = tableRow.substring(3);	
+		count = Number(count) + 1;
+		arrayNumber = Number(arrayNumber) + 1;					
+	} else {
+		// no table row
+		count = 1;
+		arrayNumber = 0;
+	}
+
+	$.ajax({
+		url: 'php_action/fetchProductData.php',
+		type: 'post',
+		dataType: 'json',
+		success:function(response) {
+			$("#addRowBtn").button("reset");			
+
+			var tr = '<tr id="row'+count+'" class="'+arrayNumber+'">'+			  				
+				'<td style="margin-left:60px;">'+
+					'<div class="form-group">'+
+
+					'<select class="form-control" name="productName[]" id="productName'+count+'" onchange=".('+count+')" required>'+
+						'<option value="">~~เลือกยาและเวชภัณท์~~</option>';
+						// console.log(response);
+						$.each(response, function(_index, value) {
+							tr += '<option value="'+value[0]+'">'+value[1]+'</option>';
+													
+						});
+													
+					tr += '</select>'+
+					'</div>'+
+				'</td>'+
+				'<td style="padding-left:37px;">'+
+					'<div class="form-group">'+
+					'<input type="number" name="quantity[]" id="quantity'+count+'" onkeyup="getTotal('+count+')" autocomplete="off" class="form-control" min="0" max="20" placeholder="จำนวน" required/>'+
+					'</div>'+
+				'</td>'+ 
+				'<td style="padding-left:1px;">'+
+					'<input type="hidden" name="totalValue[]" id="totalValue'+count+'" autocomplete="off" class="form-control" />'+	
+				'</td>'+		
+				'<td>'+
+				'<button class="btn btn-default removeProductRowBtn" type="button" onclick="removeProductRow('+count+')"><i class="fas fa-trash"></i></button>'+
+				'</td>'+
+
+			'</tr>';
+			if(tableLength > 0) {							
+				$("#productTable tbody tr:last").after(tr);
+			} else {				
+				$("#productTable tbody").append(tr);
+			}		
+
+		} // /success
+	});	// get the product data
+
+} // /add row
+
+function removeProductRow(row = null) {
+	if(row) {
+		$("#row"+row).remove();
+		subAmount();
+	} else {
+		alert('error! Refresh the page again');
+	}
+}
+// select on product data
+function getProductData(row = null) {
+	if(row) {
+		var productId = $("#productName"+row).val();		
+		if(productId == "") {
+			$("#date"+row).val("");
+			$("#quantity"+row).val("");						
+			$("#total"+row).val("");
+		} else {
+			$.ajax({
+				url: 'php_action/fetchSelectedProduct.php',
+				type: 'post',
+				data: {productId : productId},
+				dataType: 'json',
+				success:function(response) {
+					// setting the date value into the date input field
+					
+					$("#date"+row).val(response.date);
+					$("#dateValue"+row).val(response.date);
+
+					$("#quantity"+row).val(1);
+
+					var total = Number(response.date) * 1;
+					total = total.toFixed(2);
+					$("#total"+row).val(total);
+					$("#totalValue"+row).val(total);
+					
+					// check if product name is selected
+					// var tableProductLength = $("#productTable tbody tr").length;					
+					// for(x = 0; x < tableProductLength; x++) {
+					// 	var tr = $("#productTable tbody tr")[x];
+					// 	var count = $(tr).attr('id');
+					// 	count = count.substring(3);
+
+					// 	var productValue = $("#productName"+row).val()
+
+					// 	if($("#productName"+count).val() != productValue) {
+					// 		// $("#productName"+count+" #changeProduct"+count).addClass('div-hide');	
+					// 		$("#productName"+count).find("#changeProduct"+productId).addClass('div-hide');								
+					// 		console.log("#changeProduct"+count);
+					// 	}											
+					// } // /for
+					subAmount();
+				} // /success
+			}); // /ajax function to fetch the product data	
+		}
+				
+	} else {
+		alert('no row! please refresh the page');
+	}
+} // /select on product data
+
+// table total
+function getTotal(row = null) {
+	if(row) {
+		var total = Number($("#date"+row).val()) * Number($("#quantity"+row).val());
+		total = total.toFixed(2);
+		$("#total"+row).val(total);
+		$("#totalValue"+row).val(total);
+		
+		subAmount();
+
+	} else {
+		alert('no row !! please refresh the page');
+	}
+}
+
+function subAmount() {
+	var tableProductLength = $("#productTable tbody tr").length;
+	var totalSubAmount = 0;
+	for(x = 0; x < tableProductLength; x++) {
+		var tr = $("#productTable tbody tr")[x];
+		var count = $(tr).attr('id');
+		count = count.substring(3);
+
+		totalSubAmount = Number(totalSubAmount) + Number($("#total"+count).val());
+	} // /for
+
+	totalSubAmount = totalSubAmount.toFixed(2);
+
+	// sub total
+	$("#subTotal").val(totalSubAmount);
+	$("#subTotalValue").val(totalSubAmount);
+} // /sub total amount
+
+function resetOrderForm() {
+	// reset the input field
+	$("#createOrderForm")[0].reset();
+	// remove remove text danger
+	$(".text-danger").remove();
+	// remove form group error 
+	$(".form-group").removeClass('has-success').removeClass('has-error');
+} // /reset order form
+
+// remove order from server
+function removeOrder(orderId = null) {
+	if(orderId) {
+		$("#removeOrderBtn").unbind('click').bind('click', function() {
+			$("#removeOrderBtn").button('loading');
+
+			$.ajax({
+				url: 'php_action/removeOrder.php',
+				type: 'post',
+				data: {orderId : orderId},
+				dataType: 'json',
+				success:function(response) {
+					$("#removeOrderBtn").button('reset');
+
+					if(response.success == true) {
+
+						manageOrderTable.ajax.reload(null, false);
+						// hide modal
+						$("#removeOrderModal").modal('hide');
+						// success messages
+						$("#success-messages").html('<div class="alert alert-success">'+
+	            '<button type="button" class="close" data-dismiss="alert">&times;</button>'+
+	            '<strong><i class="glyphicon glyphicon-ok-sign"></i></strong> '+ response.messages +
+	          '</div>');
+
+						// remove the mesages
+	          $(".alert-success").delay(500).show(10, function() {
+							$(this).delay(3000).hide(10, function() {
+								$(this).remove();
+							});
+						}); // /.alert	          
+
+					} else {
+						// error messages
+						$(".removeOrderMessages").html('<div class="alert alert-warning">'+
+	            '<button type="button" class="close" data-dismiss="alert">&times;</button>'+
+	            '<strong><i class="glyphicon glyphicon-ok-sign"></i></strong> '+ response.messages +
+	          '</div>');
+
+						// remove the mesages
+	          $(".alert-success").delay(500).show(10, function() {
+							$(this).delay(3000).hide(10, function() {
+								$(this).remove();
+							});
+						}); // /.alert	          
+					} // /else
+
+				} // /success
+			});  // /ajax function to remove the order
+
+		}); // /remove order button clicked
+		
+
+	} else {
+		alert('error! refresh the page again');
+	}
+}
+// /remove order from server
+
+// Payment ORDER
+function paymentOrder(orderId = null) {
+	if(orderId) {
+		$.ajax({
+			url: 'php_action/fetchOrderData.php',
+			type: 'post',
+			data: {orderId: orderId},
+			dataType: 'json',
+			success:function(response) {				
+
+				// update payment
+				$("#updatePaymentOrderBtn").unbind('click').bind('click', function() {
+					var treat = $("#treat").val();
+					var paymentStatus = $("#paymentStatus").val();
+
+					if(treat == "") {
+						$("#treat").after('<p class="text-danger">The Pay Amount field is required</p>');
+						$("#treat").closest('.form-group').addClass('has-error');
+					} else {
+						$("#treat").closest('.form-group').addClass('has-success');
+					}
+
+					if(paymentStatus == "") {
+						$("#paymentStatus").after('<p class="text-danger">The Pay Amount field is required</p>');
+						$("#paymentStatus").closest('.form-group').addClass('has-error');
+					} else {
+						$("#paymentStatus").closest('.form-group').addClass('has-success');
+					}
+
+					if(treat && paymentStatus) {
+						$("#updatePaymentOrderBtn").button('loading');
+						$.ajax({
+							url: 'php_action/editPayment.php',
+							type: 'post',
+							data: {
+								orderId: orderId,
+								treat: treat,
+								paymentStatus: paymentStatus,
+							},
+							dataType: 'json',
+							success:function(response) {
+								$("#updatePaymentOrderBtn").button('loading');
+
+								// remove error
+								$('.text-danger').remove();
+								$('.form-group').removeClass('has-error').removeClass('has-success');
+
+								$("#paymentOrderModal").modal('hide');
+
+								$("#success-messages").html('<div class="alert alert-success">'+
+			            '<button type="button" class="close" data-dismiss="alert">&times;</button>'+
+			            '<strong><i class="glyphicon glyphicon-ok-sign"></i></strong> '+ response.messages +
+			          '</div>');
+
+								// remove the mesages
+			          $(".alert-success").delay(500).show(10, function() {
+									$(this).delay(3000).hide(10, function() {
+										$(this).remove();
+									});
+								}); // /.alert	
+
+			          // refresh the manage order table
+								manageOrderTable.ajax.reload(null, false);
+
+							} //
+
+						});
+					} // /if
+						
+					return false;
+				}); // /update payment			
+
+			} // /success
+		}); // fetch order data
+	} else {
+		alert('Error ! Refresh the page again');
+	}
+}
+
